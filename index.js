19ef9427549b9961f17fe214a78b03a348064be0
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-15 15:07:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract() {
+  
+}
+
+function multiply() {
+  
+}
+
+function divide() {
+  
+}
+
+function inc() {
+  
+}
+
+function dec() {
+  
+}
+
+function makeInt(n) {
+  
+}
+
+function preserveDecimal(n) {
+  
+}
