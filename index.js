1e0357a86140095b5ba200bea4c68c1aa771af21
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-15 15:11:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ function add(a, b) {
   return a + b
 }
 
-function subtract() {
+function subtract(a, b) {
   return a - b
 }
 
-function multiply() {
+function multiply(a, b) {
   return a * b
 }
 
-function divide() {
+function divide(a, b) {
   return a / b
 }
 
